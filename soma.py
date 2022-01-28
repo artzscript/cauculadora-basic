6ecae4c6ae5a166e94b92463b468ab6e69fd0a4c
Bryan Matheus <98598484+artzscript@users.noreply.github.com>
2022-01-28 13:11:15 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,18 @@
+#eu estou comecando progamacao, e essa é a minha primeira script, bem simples porem espero q goste, ano 2019.
+#ass: ArTz script
+
+
+nome = input(' vamos lá, qual o seu nome? ') 
+print('\n Olá {}!! está com problemas em resolver somas dificeis ou até mesmo simples? vou te ajudar nessa!'.format(nome, ))
+
+num1 = int(input('\n Digite um valor: '))
+num2 = int(input(' Digite mais um valor: '))
+
+a = num1 + num2
+d = num1 / num2
+m = num1 * num2 
+di = num1 // num2
+p = num1 ** num2
+
+print (' a soma é {} a divisão é {:.4f} o produto é {}'.format(a, d, m, ), end=' >>> ')
+print ('a divisão inteira é {} e a potência é {}.'.format(di, p, ))
